03b5f83fc7d10f4900d60b181512b93d801c6d66
Jeremias Pazos <jeremias.pazos@hotmail.com>
2021-02-03 17:14:18 +0000

    Tryharding 6

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -7,10 +7,10 @@
  */
 int main(void)
 {
-printf("Size of a char: %d\n byte(s)",sizeof(char));
-printf("Size of a int: %d\n byte(s)",sizeof(int));
-printf("Size of a long int: %d\n byte(s)",sizeof(long));
-printf("Size of a long long int: %d\n byte(s)",sizeof(long long));
-printf("Size of a float: %d\n byte(s)",sizeof(float));
+printf("Size of a char: %d\n byte(s)", sizeof(char));
+printf("Size of an int: %d\n byte(s)", sizeof(int));
+printf("Size of a long int: %d\n byte(s)", sizeof(long));
+printf("Size of a long long int: %d\n byte(s)", sizeof(long long));
+printf("Size of a float: %d\n byte(s)", sizeof(float));
 return (0);
 }
