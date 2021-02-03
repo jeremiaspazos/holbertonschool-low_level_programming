19dcbe3a789a29007237d489ff13a66d5f43e8f0
Jeremias Pazos <jeremias.pazos@hotmail.com>
2021-02-03 16:44:48 +0000

    Trying 5

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -7,6 +7,6 @@
  */
 int main(void)
 {
-printf("with proper grammar, but the outcome is a pice of art,")
-return (0)
+printf("with proper grammar, but the outcome is a pice of art,");
+return (0);
 }
