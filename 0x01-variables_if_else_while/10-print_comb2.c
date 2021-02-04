f6d5d4d42ce29818684367eb74d2c367b0f6474e
Jeremias Pazos <jeremias.pazos@hotmail.com>
2021-02-04 22:47:21 +0000

    10 to check

---
+++ 0x01-variables_if_else_while/10-print_comb2.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+/**
+ * main - prints  00-99
+ * Return:  0 = ok
+ */
+
+int main(void)
+{
+int i, j;
+
+for (i = 0; i < 10; i++)
+{
+for (j = 0; j < 10; j++)
+{
+
+putchar(i + '0');
+putchar(j + '0');
+if (i == 9 && j == 9)
+break;
+putchar(',');
+putchar(' ');
+}
+}
+putchar('\n');
+return (0);
+}
