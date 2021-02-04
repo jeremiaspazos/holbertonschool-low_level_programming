f6e04abb114d6ce475c129654bbdb319b74c8938
Jeremias Pazos <jeremias.pazos@hotmail.com>
2021-02-04 21:39:03 +0000

    5 to check

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - prints from 0-9 in base 10
+ * Return: 0 = ok
+ */
+
+int main(void)
+{
+int n;
+
+for (n = 0; n < 10; n++)
+printf("%d", n);
+printf("\n");
+return (0);
+}
