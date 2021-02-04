54b38b54151fdcb68d473f6eee02468a2e6b180b
Jeremias Pazos <jeremias.pazos@hotmail.com>
2021-02-04 21:51:18 +0000

    Struggling with 7

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - prints alphabet in reverse
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+int n;
+
+for (n = 122; n > 96; n--)
+putchar(n);
+putchar('\n');
+return (0);
+}
