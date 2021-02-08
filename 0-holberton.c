2075dea8a656978fcae957a1614758e040ce731b
Jeremias Pazos <jeremias.pazos@hotmail.com>
2021-02-08 14:13:42 +0000

    Setup

---
+++ 0-holberton.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <holberton.h>
+
+/**
+ *main - Print Holberton
+ *Return: 0 if everything is ok
+ */
+
+int main(void)
+{
+char n = Holberton
+_putchar(n)
+}
---
+++ README.md
@@ -0,0 +1,1 @@
+This is my proyect of C Functions and nested loops
