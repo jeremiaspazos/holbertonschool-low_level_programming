a66e7cc61a3a0b6ee491820acc3981d8b458b73a
Jeremias Pazos <jeremias.pazos@hotmail.com>
2021-02-09 17:04:34 +0000

    6 to check

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,16 @@
+#include "holberton.h"
+
+/**
+ * _abs - return  absolute value of an int
+ * @n: int to check
+ * Return: absolute value of int
+ */
+
+int _abs(int n)
+{
+if (n >= 0)
+{
+return (n);
+}
+return (-n);
+}
