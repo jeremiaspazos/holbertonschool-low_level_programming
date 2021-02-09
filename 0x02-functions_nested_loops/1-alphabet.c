9e47390da617e20a0c80d4fbb4772a0bed19a6db
Jeremias Pazos <jeremias.pazos@hotmail.com>
2021-02-09 15:49:24 +0000

    1 to check

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+#include "holberton.h"
+
+/**
+ * main - check the code for Holberton School students.
+ *
+ * Return: Always 0.
+ */
+
+void print_alphabet(void)
+{
+char al
+for(al='a';al <'z';al++)
+_putchar(al);
+_putchar('\n');
+}
