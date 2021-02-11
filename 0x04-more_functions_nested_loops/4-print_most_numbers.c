dea6f5078bb4c405fd67aa82748f369cb872f783
Jeremias Pazos <jeremias.pazos@hotmail.com>
2021-02-11 17:53:41 +0000

    4 to check

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,25 @@
+#include "holberton.h"
+
+
+/**
+ * print_most_numbers - Entry point
+ *
+ * Description: Sasdasdsa
+ * Return: Always 0 (Success)
+ */
+
+void print_most_numbers(void);
+
+int numero
+for (numero = 0; numero < 10; numero++)
+{
+if(numero == '2' || numero '4')
+{
+continue;
+}
+else;
+{
+  _putchar ('0' + numero);
+}
+}
+_putchar('\n');
