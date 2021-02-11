261a153bcd45d606fb67e88244415d86e2fc5715
Jeremias Pazos <jeremias.pazos@hotmail.com>
2021-02-11 17:38:02 +0000

    3

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+
+/**
+ * print_numbers - Entry point
+ *
+ * Description: prints from 0 to 9 with a line
+ * Return: Always 0 (Success)
+ */
+
+void print_numbers(void)
+{
+int a
+
+for (a = 0; a < 10; a++)
+_putchar (a);
+_putchar ('\n');
+return (0);
+}
