182f78e39ee8921f81cfbd0bc7217599dfcb56f3
Jeremias Pazos <jeremias.pazos@hotmail.com>
2021-02-11 17:30:38 +0000

    2

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+#include "holberton.h"
+
+/**
+ * mul - entry point
+ * @a: number
+ * @b: number
+ * Description: Multiplies a and b
+ * Return: a*b
+ */
+
+int mul(int a, int b)
+{
+return (a * b);
+}
