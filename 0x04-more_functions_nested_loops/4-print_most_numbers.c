56becfe740ba473b45e81bf74588b841e0e06ed4
Jeremias Pazos <jeremias.pazos@hotmail.com>
2021-02-11 17:59:04 +0000

    444

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -8,13 +8,14 @@
  * Return: Always 0 (Success)
  */
 
-void print_most_numbers(void);
+void print_most_numbers(void)
 {
-  int numero;
-for (numero = 0; numero < 10; numero++)
+int numero;
+
+for (numero = 0; numero <= 9; numero++)
 {
-if (numero != 2 || numero != 4)
-_putchar ('0' + numero);
-_putchar('\n');
+if (numero != 2 && numero != 4)
+_putchar('0' + numero);
 }
+_putchar('\n');
 }
