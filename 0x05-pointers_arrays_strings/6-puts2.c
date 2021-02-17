8083a254b6a2510e8f293d417a40f09e1b9b2506
Jeremias Pazos <jeremias.pazos@hotmail.com>
2021-02-17 21:30:09 +0000

    jJJ

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,24 @@
+#include "holberton.h"
+
+/**
+ * puts2 - entrypoint
+ *  * @str: string
+ */
+void puts2(char *str)
+{
+int len, i;
+
+len = 0;
+
+while (str[len] != '\0')
+{
+len++;
+}
+
+for (i = 0; i < len; i += 2)
+{
+_putchar(str[i]);
+}
+
+_putchar('\n');
+}
